ae990882ec9aa8970db43ff179ac2ce9bbd50935
Schwems <sven001jaeger@gmail.com>
2024-11-16 16:09:13 +0100

    minimal working javafx UI

--- src/main/java/de/fh/albsig/MainApp.java
+++ src/main/java/de/fh/albsig/MainApp.java
@@ -1,4 +1,28 @@
 package de.fh.albsig;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Debugging: Zeige den Pfad zur FXML-Datei an
+        System.out.println("FXML-URL: " + getClass().getResource("/de/fh/albsig/view/MainView.fxml"));
+
+        // Lade die FXML-Datei
+        Parent root = FXMLLoader.load(getClass().getResource("/de/fh/albsig/view/MainView.fxml"));
+
+        // Setze die Szene und zeige das Fenster
+        primaryStage.setTitle("JavaFX Minimal App");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
