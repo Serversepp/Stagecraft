50ee877f482ba6a5f12ee69a6e6e83e14d25d089
Serversepp <53191095+Serversepp@users.noreply.github.com>
2024-11-16 12:17:39 +0100

    removed print

--- src/main/java/de/fh/albsig/Main.java
+++ src/main/java/de/fh/albsig/Main.java
@@ -3,7 +3,6 @@ package de.fh.albsig;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Helloo world!");
         // Create and show the UI
         Ui ui = new Ui();
         ui.show();
