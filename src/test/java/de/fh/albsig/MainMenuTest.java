b16431c0dfcd5fc1cc5aee35f868dca543ef69c4
Serversepp <53191095+Serversepp@users.noreply.github.com>
2025-01-22 23:26:02 +0100

    Update MainMenuTest.java
    added missing import statement

--- src/test/java/de/fh/albsig/MainMenuTest.java
+++ src/test/java/de/fh/albsig/MainMenuTest.java
@@ -16,6 +16,7 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.condition.EnabledIf;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static org.mockito.Mockito.*;
 
